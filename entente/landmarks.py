d322dd0b93ed0d1f27c1e83d7ba1e0ebe9c96e7b
Paul Melnikow <email@paulmelnikow.com>
2018-09-30 14:38:51 -0400

    Reformat using black

--- entente/landmarks.py
+++ entente/landmarks.py
@@ -1,8 +1,10 @@
 from cached_property import cached_property
 
+
 class Landmarker(object):
     def __init__(self, source_mesh, landmarks):
         from cgal_search import require_cgal
+
         require_cgal()
         self.source_mesh = source_mesh
         self.landmarks = landmarks
@@ -11,9 +13,11 @@ class Landmarker(object):
     def load(cls, source_mesh_path, landmark_path):
         from lace.mesh import Mesh
         from lace.serialization import meshlab_pickedpoints
+
         return cls(
             source_mesh=Mesh(filename=source_mesh_path),
-            landmarks=meshlab_pickedpoints.load(landmark_path))
+            landmarks=meshlab_pickedpoints.load(landmark_path),
+        )
 
     @cached_property
     def regressor(self):
@@ -28,20 +32,23 @@ class Landmarker(object):
         face_indices = faces_nearest_to_points(self.source_mesh, landmark_points)
         vertex_indices = self.source_mesh.f[face_indices]
         coords = compute_barycentric_coordinates(
-            self.source_mesh.v[vertex_indices],
-            landmark_points)
+            self.source_mesh.v[vertex_indices], landmark_points
+        )
 
-        row_indices = np.repeat(np.arange(3*num_landmarks, step=3), 3) \
-            .reshape(-1, 1) + np.arange(3, dtype=np.uint64)
-        column_indices = 3 * np.repeat(vertex_indices, 3) \
-            .reshape(-1, 3) + np.arange(3, dtype=np.uint64)
+        row_indices = np.repeat(np.arange(3 * num_landmarks, step=3), 3).reshape(
+            -1, 1
+        ) + np.arange(3, dtype=np.uint64)
+        column_indices = 3 * np.repeat(vertex_indices, 3).reshape(-1, 3) + np.arange(
+            3, dtype=np.uint64
+        )
         tiled_coords = np.tile(coords.reshape(-1, 1), 3)
         return sparse(
             row_indices.flatten(),
             column_indices.flatten(),
             tiled_coords.flatten(),
-            3*num_landmarks,
-            3*self.source_mesh.v.shape[0])
+            3 * num_landmarks,
+            3 * self.source_mesh.v.shape[0],
+        )
 
     def invoke_regressor(self, target):
         coords = self.regressor * target.v.reshape(-1)
@@ -49,6 +56,7 @@ class Landmarker(object):
 
     def transfer_landmarks_onto(self, target):
         from .equality import have_same_topology
+
         if not have_same_topology(self.source_mesh, target):
             # raise ValueError('Target mesh must have the same topology')
             target.f = self.source_mesh.f
--- entente/validation.py
+++ entente/validation.py
@@ -7,39 +7,34 @@ def validate_shape(a, *shape, **kwargs):
 
     Return: The wildcard dimension or a tuple of wildcard dimensions.
     """
-    is_wildcard = lambda dim: dim == '*'
+    is_wildcard = lambda dim: dim == "*"
     if all(not isinstance(dim, int) and not is_wildcard(dim) for dim in shape):
         raise ValueError("Expected shape dimensions to be int or '*'")
 
-    if 'name' in kwargs:
-        preamble = '{} must be an array'.format(kwargs['name'])
+    if "name" in kwargs:
+        preamble = "{} must be an array".format(kwargs["name"])
     else:
-        preamble = 'Expected an array'
+        preamble = "Expected an array"
 
     if a is None:
-        raise ValueError('{} with shape {}; got None'.format(preamble, shape))
+        raise ValueError("{} with shape {}; got None".format(preamble, shape))
     try:
         len(a.shape)
-    except AttributeError, TypeError:
-        raise ValueError('{} with shape {}; got {}'.format(
-            preamble, shape, a.__class__))
+    except (AttributeError, TypeError):
+        raise ValueError(
+            "{} with shape {}; got {}".format(preamble, shape, a.__class__)
+        )
 
     # Check non-wildcard dimensions.
-    if len(a.shape) != len(shape) or \
-        any(
-            actual != expected
-            for actual, expected
-            in zip(a.shape, shape)
-            if not is_wildcard(expected)
-        ):
-        raise ValueError('{} with shape {}; got {}'.format(
-            preamble, shape, a.shape))
+    if len(a.shape) != len(shape) or any(
+        actual != expected
+        for actual, expected in zip(a.shape, shape)
+        if not is_wildcard(expected)
+    ):
+        raise ValueError("{} with shape {}; got {}".format(preamble, shape, a.shape))
 
     wildcard_dims = [
-        actual
-        for actual, expected
-        in zip(a.shape, shape)
-        if is_wildcard(expected)
+        actual for actual, expected in zip(a.shape, shape) if is_wildcard(expected)
     ]
     if len(wildcard_dims) == 0:
         return None
@@ -47,6 +42,7 @@ def validate_shape(a, *shape, **kwargs):
         return wildcard_dims[0]
     else:
         return tuple(wildcard_dims)
+
 
 def validate_shape_from_ns(namespace, name, *shape):
     """
