125bf095ce5277731e1f29fd886cbbf4a6294d31
Paul Melnikow <email@paulmelnikow.com>
2018-10-01 13:43:37 -0400

    WIP

---
+++ entente/restore_correspondence.py
@@ -0,0 +1,12 @@
+DEFAULT_COMPARATOR = lambda a, b: all(a == b)
+
+def find_correspondence(a, b, cmp=DEFAULT_COMPARATOR):
+    """
+    Given two orderings of identical elements, return an array that maps
+    indices of the first to the indices of the second.
+    """
+    if not len(a) == len(b):
+        raise
+
+def restore_correspondence():
+
