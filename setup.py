fe05108da9a362d816988fd944c0a0e6e9c4c2e2
Paul Melnikow <email@paulmelnikow.com>
2018-10-01 11:42:17 -0400

    More formatting

--- setup.py
+++ setup.py
@@ -1,32 +1,32 @@
 import importlib
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     install_requires = f.read()
 
 setup(
-    name='entente',
-    version=importlib.import_module('entente').__version__,
-    description='Library for working with polygonal meshes which have vertex-wise correspondence',
+    name="entente",
+    version=importlib.import_module("entente").__version__,
+    description="Library for working with polygonal meshes which have vertex-wise correspondence",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    author='Metabolize',
-    author_email='github@paulmelnikow.com',
-    url='https://github.com/metabolize/entente',
+    long_description_content_type="text/markdown",
+    author="Metabolize",
+    author_email="github@paulmelnikow.com",
+    url="https://github.com/metabolize/entente",
     packages=find_packages(),
     install_requires=install_requires,
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Other Audience',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Artistic Software',
-        'Topic :: Multimedia :: Graphics :: 3D Modeling',
-        'Topic :: Scientific/Engineering :: Visualization'
-    ]
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Other Audience",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Artistic Software",
+        "Topic :: Multimedia :: Graphics :: 3D Modeling",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
 )
