528966168b27f2c4714dc7efe0344e64c661d1b0
Paul Melnikow <email@paulmelnikow.com>
2018-10-02 15:47:48 -0400

    Properly install git-lfs in container

--- docker/entente-ci/install_container_dependencies.sh
+++ docker/entente-ci/install_container_dependencies.sh
@@ -1,5 +1,7 @@
 apt-get update
 
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+
 apt-get install -y --no-install-recommends \
   python-opencv \
   libsuitesparse-dev \
