6a787ce5d641226f891a2bfcaa5e0d5a7b1f7ce5
Paul Melnikow <email@paulmelnikow.com>
2019-04-03 13:10:45 -0400

    Reformat

--- entente/test_landmarks.py
+++ entente/test_landmarks.py
@@ -11,18 +11,18 @@ def test_landmarker():
     cube_2.translate(np.array([0, 3.5, 1.0]))
 
     landmarks = {
-        'origin': np.zeros(3),
-        'near_opposite_corner': np.array([0.8, 0.9, 1.0]),
+        "origin": np.zeros(3),
+        "near_opposite_corner": np.array([0.8, 0.9, 1.0]),
     }
 
     landmarker = Landmarker(cube_1, landmarks)
 
     transferred = landmarker.transfer_landmarks_onto(cube_2)
     expected = {
-        'origin': np.array([0.0, 3.5, 1.0]),
-        'near_opposite_corner': np.array([4.0, 8.0, 6.0]),
+        "origin": np.array([0.0, 3.5, 1.0]),
+        "near_opposite_corner": np.array([4.0, 8.0, 6.0]),
     }
-    np.testing.assert_array_equal(transferred['origin'], expected['origin'])
-    np.testing.assert_array_equal(transferred['near_opposite_corner'], expected['near_opposite_corner'])
-
-
+    np.testing.assert_array_equal(transferred["origin"], expected["origin"])
+    np.testing.assert_array_equal(
+        transferred["near_opposite_corner"], expected["near_opposite_corner"]
+    )
