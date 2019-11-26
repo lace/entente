2faf5735fabe5c852780cb07945fa20b9b50094f
Paul Melnikow <email@paulmelnikow.com>
2019-11-25 19:48:30 -0500

    Update classifier

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Topic :: Artistic Software",
         "Topic :: Multimedia :: Graphics :: 3D Modeling",
