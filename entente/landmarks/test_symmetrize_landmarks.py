f352c00021239dd67b70641b49e83073fdb74eb5
Paul Melnikow <email@paulmelnikow.com>
2019-12-27 15:00:21 -0500

    100% coverage

--- entente/landmarks/test_symmetrize_landmarks.py
+++ entente/landmarks/test_symmetrize_landmarks.py
@@ -25,6 +25,12 @@ def test_symmetrize_landmarks_using_plane():
     np.testing.assert_allclose(distances_to_original, distances_to_mirrored, atol=1e-1)
 
 
+def test_symmetrize_landmarks_using_plane_non_plane():
+    original = np.array([[-18.5657, 54.7161, -19.5649], [20.0896, 54.919, -19.5738]])
+    with pytest.raises(ValueError, match=r"plane_of_symmetry should be a Plane"):
+        symmetrize_landmarks_using_plane("not_a_plane", original)
+
+
 def test_symmetrize_landmarks_using_topology():
     mesh = create_seat_and_arm_mesh()
     original = np.array([[-18.5657, 54.7161, -19.5649], [20.0896, 54.919, -19.5738]])
@@ -52,3 +58,11 @@ def test_symmetrize_landmarks_using_topology_asymmetrical():
         ValueError, match=r"Some landmarks are near triangles which are not mirrored"
     ):
         symmetrize_landmarks_using_topology(mesh, Plane.yz, original, atol=1e-1)
+
+
+def test_symmetrize_landmarks_using_topology_non_plane():
+    mesh = create_seat_and_arm_mesh()
+    original = np.array([[-18.5657, 54.7161, -19.5649], [20.0896, 54.919, -19.5738]])
+    mesh.translate(np.array([50.0, 0.0, 0.0]))
+    with pytest.raises(ValueError, match=r"plane_of_symmetry should be a Plane"):
+        symmetrize_landmarks_using_topology(mesh, "not_a_plane", original, atol=1e-1)
