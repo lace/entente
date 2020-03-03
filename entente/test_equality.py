289a40ba98e3e1a751cf045d3e2fffaca0840b13
Paul Melnikow <email@paulmelnikow.com>
2020-03-03 16:48:07 -0500

    Do not use lace.shapes

--- entente/test_equality.py
+++ entente/test_equality.py
@@ -1,24 +1,24 @@
+from lacecore import shapes
+import numpy as np
 from .equality import have_same_topology
 
 
 def test_have_same_topology():
-    import numpy as np
-    from lace.shapes import create_cube
 
-    cube_1 = create_cube(np.zeros(3), 1.0)
-    cube_2 = create_cube(np.zeros(3), 1.0)
+    cube_1 = shapes.cube(np.zeros(3), 1.0)
+    cube_2 = shapes.cube(np.zeros(3), 1.0)
     assert have_same_topology(cube_1, cube_2) is True
 
-    cube_1 = create_cube(np.zeros(3), 1.0)
-    cube_2 = create_cube(np.ones(3), 1.0)
+    cube_1 = shapes.cube(np.zeros(3), 1.0)
+    cube_2 = shapes.cube(np.ones(3), 1.0)
     assert have_same_topology(cube_1, cube_2) is True
 
-    cube_1 = create_cube(np.zeros(3), 1.0)
-    cube_2 = create_cube(np.zeros(3), 1.0)
+    cube_1 = shapes.cube(np.zeros(3), 1.0)
+    cube_2 = shapes.cube(np.zeros(3), 1.0)
     cube_2.f = np.roll(cube_2.f, 1, axis=1)
     assert have_same_topology(cube_1, cube_2) is False
 
-    cube_1 = create_cube(np.zeros(3), 1.0)
-    cube_2 = create_cube(np.zeros(3), 1.0)
-    del cube_2.f
+    cube_1 = shapes.cube(np.zeros(3), 1.0)
+    cube_2 = shapes.cube(np.zeros(3), 1.0)
+    cube_2.f = np.zeros((0, 3), dtype=np.uint64)
     assert have_same_topology(cube_1, cube_2) is False
