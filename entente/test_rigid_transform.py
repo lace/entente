ac60dea6937900198dec2131578719b53ce21bcf
Jonathan Levin <jon@curvewise.com>
2022-12-15 16:47:18 -0500

    fix test

--- entente/test_rigid_transform.py
+++ entente/test_rigid_transform.py
@@ -98,25 +98,23 @@ def test_rigid_rotation_with_reflection():
 
 def test_numpy_regression():
     array = np.array([[-4.0, -2.0, -2.0], [-2.0, -4.0, -2.0], [-2.0, -2.0, -4.0]])
-    expected_result = (
-        np.array(
-            [
-                [-5.77350269e-01, 8.16496581e-01, -5.04179082e-17],
-                [-5.77350269e-01, -4.08248290e-01, -7.07106781e-01],
-                [-5.77350269e-01, -4.08248290e-01, 7.07106781e-01],
-            ]
-        ),
-        array([8.0, 2.0, 2.0]),
-        array(
-            [
-                [0.57735027, 0.57735027, 0.57735027],
-                [-0.81649658, 0.40824829, 0.40824829],
-                [-0.0, 0.70710678, -0.70710678],
-            ]
-        ),
+    expected_a = np.array(
+        [
+            [-5.77350269e-01, 8.16496581e-01, -5.04179082e-17],
+            [-5.77350269e-01, -4.08248290e-01, -7.07106781e-01],
+            [-5.77350269e-01, -4.08248290e-01, 7.07106781e-01],
+        ]
     )
-    result = np.linalg.svd(array, full_matrices=False)
-    np.testing.assert_array_equal(result, expected_result)
+    expected_b = np.array(
+        [
+            [0.57735027, 0.57735027, 0.57735027],
+            [-0.81649658, 0.40824829, 0.40824829],
+            [-0.0, 0.70710678, -0.70710678],
+        ]
+    )
+    (a, _, b) = np.linalg.svd(array, full_matrices=False)
+    np.testing.assert_almost_equal(expected_a, a)
+    np.testing.assert_almost_equal(expected_b, b)
 
 
 def test_rigid_rotation_with_scale():
