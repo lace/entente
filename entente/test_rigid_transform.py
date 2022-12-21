1a412c04d0fb132ef3170410fb0e99ed1f0517b0
Jonathan Levin <jon@curvewise.com>
2022-12-21 12:53:12 -0500

    reformat and fix import order

--- entente/test_rigid_transform.py
+++ entente/test_rigid_transform.py
@@ -1,5 +1,5 @@
-from env_flag import env_flag
 from entente.rigid_transform import find_rigid_rotation, find_rigid_transform
+from env_flag import env_flag
 import numpy as np
 from polliwog import Box
 from polliwog.transform import euler
@@ -88,9 +88,7 @@ def test_rigid_rotation_single_point():
     np.testing.assert_array_almost_equal(a.dot(R), b)
 
 
-@pytest.mark.skipif(
-    env_flag('CI') is True, reason="failing in CI when numpy>=1.19.3"
-)
+@pytest.mark.skipif(env_flag("CI") is True, reason="failing in CI when numpy>=1.19.3")
 def test_rigid_rotation_with_reflection():  # pragma: nocover
     a = Box(origin=np.array([0.0, 0.0, 0.0]), size=np.array([1.0, 1.0, 1.0])).v
     b = a * -1
